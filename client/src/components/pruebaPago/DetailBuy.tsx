8febd3fb7c4f88d3731beaa0eb3e05cbed4419a9
Esteban Velasquez <jestebanvelasquez22@gmail.com>
2022-08-23 18:12:25 -0500

    pagos creando

--- client/src/components/pruebaPago/DetailBuy.tsx
+++ client/src/components/pruebaPago/DetailBuy.tsx
@@ -82,13 +82,6 @@ export default function DetailBuy( {event} ) {
                             {/* ) */}
                         {/* })} */}
                     </div>
-                {/* </> : <div className="flex flex-col items-center my-10">
-                    <p className="text-4xl font-light">Este artista no tiene eventos disponibles</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-36 w-36" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                </div> */}
-            {/* } */}
         </>
     )
 }
